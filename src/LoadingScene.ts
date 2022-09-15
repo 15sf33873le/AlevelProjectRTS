4d3d6e49596e3219fd9c2097f13dce7465a042c3
Louie Elford <louie.elford@gmail.com>
2022-09-15 18:46:53 +0100

    added loading screen

--- src/LoadingScene.ts
+++ src/LoadingScene.ts
@@ -17,10 +17,90 @@ export default class LoadingScene extends Phaser.Scene {
 	}
 
 	preload() {
+		
+		//load the phaser3 logo a bunch of times to simulate loading
+		this.load.image('test', 'assets/libs.png');
 		this.load.image('logo', 'assets/phaser3-logo.png');
+		for (let i = 0; i < 500; i++) {
+			this.load.image('logo'+i, 'assets/phaser3-logo.png');
+		}
+		
+		//create the loading progress bar
+		let progressBar = this.add.graphics();
+		let progressBox = this.add.graphics();
+		progressBox.fillStyle(0x222222, 0.8);
+		progressBox.fillRect(240, 270, 320, 50);
+
+		//add loading... text
+		let width = this.cameras.main.width;
+		let height = this.cameras.main.height;
+		let loadingText = this.make.text({
+			x: width / 2,
+			y: height / 2 - 50,
+			text: 'Loading...',
+			style: {
+				font: '20px monospace',
+				color: '#ffffff'
+			}
+		});
+		loadingText.setOrigin(0.5, 0.5);
+
+		//add loading percentage text
+		let percentText = this.make.text({
+			x: width / 2,
+			y: height / 2 - 5,
+			text: '0%',
+			style: {
+				font: '18px monospace',
+				color: '#ffffff'
+			}
+		});
+		percentText.setOrigin(0.5, 0.5);
+
+		//add asset loading text which will display the asset currently loading
+		let assetText = this.make.text({
+			x: width / 2,
+			y: height / 2 + 50,
+			text: '',
+			style: {
+				font: '18px monospace',
+				color: '#ffffff'
+			}
+		});
+		assetText.setOrigin(0.5, 0.5);
+		
+		//listen for loading progress
+		this.load.on('progress', function (value) {
+			//update the loading bar
+			//console.log(value);
+			progressBar.clear();
+			progressBar.fillStyle(0xffffff, 1);
+			progressBar.fillRect(250, 280, 300 * value, 30);
+			//update the percentage text
+			percentText.setText(Math.floor(value * 100) + '%');
+		});
+		
+		//listen for file progress
+		this.load.on('load', function (file) {
+			//update the loading file text with file being loaded
+			console.log(file.src);
+			assetText.setText('Loading asset: ' + file.key);
+		});
+
+		//listen for loading complete
+		this.load.on('complete', function () {
+			//destroy loading graphics
+			console.log('complete');
+			progressBar.destroy();
+			progressBox.destroy();
+			loadingText.destroy();
+			percentText.destroy();
+			assetText.destroy();
+		});
 	}
 
 	create() {
-		let logo = this.add.image(200, 200, 'logo');
+		//create the logo graphic
+		let logo = this.add.image(400, 300, 'logo');
 	}
 }
--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,8 @@
-export default const config = {
+const config = {
     type: Phaser.AUTO,
     backgroundColor: '#125555',
     width: 800,
     height: 600,
     scene: null
 };
+export default config;
