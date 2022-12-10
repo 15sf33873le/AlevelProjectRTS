b431a02db35113849ce5cfdce346547b5f05aa5a
Louie Elford <louie.elford@gmail.com>
2022-12-10 16:04:17 +0000

    partial development of complex move orders

--- src/GameRun.ts
+++ src/GameRun.ts
@@ -1,4 +1,5 @@
-import Ship from './Ship';
+import 'phaser';
+import './Ship';
 
 const config = {
   key: "GameRun",
@@ -13,23 +14,55 @@ const config = {
   // input: {}
 };
 
-export default class GameRun extends Phaser.Scene {
-	constructor(config) {
-		super(config);
-	}
 
+let graphics: Phaser.GameObjects.Graphics;
+
+export default class GameRun extends Phaser.Scene {
+
+	public shiplist = [];
 
 	create() {
+
 		console.log('gameRun active');
-		console.log(this.cache.json.get('TestShipJSON'));
-		this.add.existing(Object.assign(new Ship(this,500,500,"TestShipSpritesheet"), this.cache.json.get('TestShipJSON')));
-		//this.sys.updateList.add(TestShip1);
-		//this.sys.displayList.add(TestShip1);
-		console.log(this.sys.updateList);
-		//console.log(TestShip1);
+		let shipName = "testship";
+		this.shiplist.push(this.add[shipName](500, 500, "TestShipSpritesheet"));
+		console.log(this.shiplist[0]);
+		
+		const point1 = new Phaser.Math.Vector2(200,200);
+		const ang1 = -115;
+		const point2 = new Phaser.Math.Vector2(400,100);
+		const ang2 = 0;
+		
+		this.shiplist[0].NewPath(point1,ang1,point2,ang2);
+
+		const point3 = new Phaser.Math.Vector2(400,100);
+		const ang3 = 0;
+		const point4 = new Phaser.Math.Vector2(900,800);
+		const ang4 = 70;
+
+		this.shiplist[0].NewPath(point3,ang3,point4,ang4);
+
+		const path3Point1 = new Phaser.Math.Vector2(900,800);
+		const path3Ang1 = 70;
+		const path3Point2 = new Phaser.Math.Vector2(300,400);
+		const path3Ang2 = -80;
+
+		this.shiplist[0].NewPath(path3Point1,path3Ang1,path3Point2,path3Ang2);
+
+		graphics = this.add.graphics();
+		console.log(this);
 	}
 	
 	update() {
-		
+		graphics.clear();
+
+		graphics.lineStyle(2, 0xffffff, 1);
+
+		if (!this.shiplist[0].moveOrderQueue.isEmpty) {
+			const lineToDraw = this.shiplist[0].moveOrderQueue.peek();
+			lineToDraw.draw(graphics);
+		}
+
+		graphics.fillStyle(0xff0000, 1);
 	}
 }
--- src/Ship.ts
+++ src/Ship.ts
@@ -1,39 +1,154 @@
+import 'phaser';
+
+class Queue {
+	
+	private elements = {};
+	private head = 0;
+	private tail = 0;
+
+	enqueue(element) {
+		this.elements[this.tail] = element;
+		this.tail++;
+	}
+	dequeue() {
+		const item = this.elements[this.head];
+		this.elements[this.head].destroy();
+		delete this.elements[this.head];
+		this.head++;
+		return item;
+	}
+	peek() {
+		return this.elements[this.head];
+	}
+	get length() {
+		return this.tail - this.head;
+	}
+	get isEmpty() {
+		return this.length === 0;
+	}
+}
+
+class MovePath extends Phaser.Curves.Path {
+
+	constructor(point1: Phaser.Math.Vector2, ang1: number, point2: Phaser.Math.Vector2, ang2: number, TurnR: number) {
+		//call the constructor from the Path class
+		super(point1.x,point1.y);
+		
+		//find the angle between the two points
+		const travelAngle: number = Phaser.Math.Angle.BetweenPoints(point1, point2) * (180/Math.PI);
+		
+		//find turn directions
+		
+		//if ang1-travelAngle < 0 clockwise
+		let turn1DIR: boolean = (ang1 - travelAngle < 0);
+		//if travelAngle-ang2 < 0 clockwise
+		const turn2DIR: boolean = (travelAngle - ang2 < 0);
+
+		//assigns the first turning circle center to point1 and the transformation from original point1 to the first turning circle center in CenterTranslation1
+		const CenterTranslation1 = MovePath.FindTurningCircle(point1,ang1,TurnR,turn1DIR);	
+		
+		
+		//assigns the second turning circle center to point2 and the transformation from original point2 to the second turning circle center in CenterTranslation2
+		const CenterTranslation2 = MovePath.FindTurningCircle(point2,ang2,TurnR,turn2DIR);
+		
+		//find the translation between the two circle centers (point1 and point2)
+		const interCenterTranslation = point2.clone();
+		interCenterTranslation.subtract(point1);
+
+		this.ellipseTo(50,50,0,interCenterTranslation.angle()*(180/Math.PI)-ang1,false,ang1-90);
+
+		console.log("end:");
+		console.log(interCenterTranslation.angle()*(180/Math.PI)-ang1)
+		console.log("rotation:");
+		console.log(ang1-90);
+		console.log("clockwise?:");
+		console.log(turn1DIR);
+
+		//create the straight section between the two turns
+		const StraightTranslation = interCenterTranslation.clone();
+		StraightTranslation.add(this.getEndPoint());
+		console.log(StraightTranslation);
+		this.lineTo(StraightTranslation);
+
+		
+		//create the second elipse curve representing turn2
+		this.ellipseTo(TurnR,TurnR,interCenterTranslation.angle()*(180/Math.PI)-90,ang2-90,!turn2DIR,0);
+
+		console.log(this.getEndPoint());
+	}//phaser.math.angle.BetweenPoints(interCenterTranslation,)
+
+	//determines the location of a turning circle
+	static FindTurningCircle(point: Phaser.Math.Vector2, ang: number, TurnR: number, TurnDir: boolean) {
+		
+		const Translation = new Phaser.Math.Vector2(TurnR,0);
+		
+		const tempAngle = (TurnDir ? ang + 90 : ang - 90)*(Math.PI/180);
+		
+		Translation.setAngle(tempAngle);
+		
+		point.add(Translation);
+		
+		return Translation;
+	}
+
+	getPointDistance(d: number) {
+		let outPoint = new Phaser.Math.Vector2();
+		let outDirection = new Phaser.Math.Vector2();
+
+        const curveLengths = this.getCurveLengths();
+        let i = 0;
+
+        while (i < curveLengths.length)
+        {
+            if (curveLengths[i] >= d)
+            {
+                var diff = curveLengths[i] - d;
+                var curve = this.curves[i];
+
+                var segmentLength = curve.getLength();
+                var u = (segmentLength === 0) ? 0 : 1 - diff / segmentLength;
+
+				return [curve.getPointAt(u, outPoint), curve.getTangentAt(u, outDirection)];
+            }
+
+            i++;
+        }
+
+        // loop where sum != 0, sum > d , sum+1 <d
+        return null;
+	}
+
+}
+
 export default class Ship extends Phaser.GameObjects.Sprite {
 
-	public directionVector: number[] = [0,-1];
 	public velocity = 0;
 	public accelTime = 0;
 	public limit = 0; //(Math.E-1)/this.accelcoef
 	public maxV = 0;
 	public accelCoef = 0;
-	public armorX: number[];
-	public armorY: number[];
-	public armorvX: number[];
-	public armorvY: number[];
+	public moveOrderQueue = new Queue();
+	public TurnR = 0;
+	public TempDisplacement: number = 0;
 
 	constructor(scene: Phaser.Scene, x: number, y: number, texture: string, frame?: string | number ) {
 		super(scene, x, y, texture, frame);
-		this.setOrigin(0, 0);
-		scene.events.on('update', this.update, this)
+		return Object.assign(this, scene.cache.json.get('TestShipJSON'));
 	}
 
-	SetVelocity(): void {
-		this.velocity = this.maxV*(Math.E*Math.log1p(this.accelCoef*this.accelTime)-this.accelCoef*this.accelTime);
-		console.log(this.velocity);
+	Accelerate(): number{
+		return this.velocity = this.maxV*(Math.E*Math.log1p(this.accelCoef*this.accelTime)-this.accelCoef*this.accelTime);
 	}	// velocity = v*(e*Math.log1p(a*x)-a*x)
 
-	Accelerate(acc: number, delta: number): void {
+	UpdateAccelTime(acc: boolean, delta: number): void {
 		
 		let incTime: number;
 
-		switch(acc) {
-			case 0:
-				break;
-			case 1:
-				incTime = this.accelTime + delta/1000;
-				break;
-			case 2:
-				incTime = this.accelTime - delta/1000;
+		if (acc) {
+			incTime = this.accelTime + delta/1000;
+		} 
+		else {
+			incTime = this.accelTime - delta/1000;
 		}
 
 		if (incTime < this.limit && incTime > 0) {
@@ -41,14 +156,43 @@ export default class Ship extends Phaser.GameObjects.Sprite {
 		}
 	}
 
-	Move(acc: number = 0, delta: number): void {
-		this.Accelerate(acc, delta);
-		this.SetVelocity();
-		console.log(this.velocity);
-		this.x = this.x + delta/1000 * (this.directionVector[0] * this.velocity);
-		this.y = this.y + delta/1000 * (this.directionVector[1] * this.velocity);
-	} // acc is true for positive acceleration and false for negative acceleration
+	Move(delta: number): void {
+		if (this.moveOrderQueue.isEmpty) return;
+		const moveOrder = this.moveOrderQueue.peek();
+		this.UpdateAccelTime(true,delta);
+		this.TempDisplacement = this.TempDisplacement + this.Accelerate() * (delta/1000);
+		const commands = moveOrder.getPointDistance(this.TempDisplacement);
+		if (!commands) {
+			this.moveOrderQueue.dequeue();
+			this.TempDisplacement = 0
+			return;
+		}
+
+		this.x = commands[0].x;
+		this.y = commands[0].y;
+		
+		this.setRotation(commands[1].angle()+Math.PI/2);
+	}
+	NewPath(point1, ang1, point2, ang2): void{
+		const PendingOrder = new MovePath(point1,ang1,point2,ang2,this.TurnR)
+		let drawnPath = new Phaser.GameObjects.Graphics(this.scene);
+		drawnPath.lineStyle(5, 0xFF00FF, 1.0);
+		PendingOrder.draw(drawnPath);
+		this.moveOrderQueue.enqueue(PendingOrder);
+	}
 	update(time, delta) {
-		this.Move(1,delta);
+		this.Move(delta);
 	}
 }
+
+Phaser.GameObjects.GameObjectFactory.register('testship', function (this: Phaser.GameObjects.GameObjectFactory, x, y, key, frame) {
+		console.log("making testship");
+		const testship = new Ship(this.scene, x, y, key, frame);
+		testship.setOrigin(0.5, 0.5);
+		testship.setScale(0.2);
+		console.log(testship);
+		this.displayList.add(testship);
+		this.updateList.add(testship);
+		this.scene.events.on('update', testship.update, testship);
+		return testship;
+});
